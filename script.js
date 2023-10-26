8fa16c12ddf270ca27c88c7eaf3d8a0a0d7c89e9
yole <igbavagodswill4@gmail.com>
2023-10-26 11:05:25 +0100

    Just finished creating the mobile nav bar

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// default variables
+const navBar = document.querySelector('.navBar');
+const mobileNav = document.querySelector('.mobileNavContainer');
+const openNav = document.querySelector('.openNav');
+const closeNav = document.querySelector('.closeNav');
+
+
+openNav.addEventListener('click', function(){
+    navBar.style.display = 'none';
+    mobileNav.style.display = 'flex';
+    openNav.classList.toggle('displayNone');
+    closeNav.classList.toggle('displayNone');
+})
+
+closeNav.addEventListener('click', function(){
+    navBar.style.display = 'flex';
+    mobileNav.style.display = 'none';
+    closeNav.classList.toggle('displayNone');
+    openNav.classList.toggle('displayNone');
+})
